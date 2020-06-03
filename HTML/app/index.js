70cd8366adc9804fef841f7817f53dde47e4a9be
kartovitskii <kartovitskii@yandex.ru>
2020-06-03 14:12:31 +0300

    Finish Index, start Detail page

--- HTML/app/index.js
+++ HTML/app/index.js
@@ -1,1 +1,3 @@
 import "@/styles/main.scss";
+
+import "@/js/test.js";
---
+++ HTML/app/js/test.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var swiper = new Swiper('.swiper-container', {
+      width: 170,
+      spaceBetween: 25,
+      centeredSlides: false,
+      slidesOffsetBefore: 25,
+      slidesOffsetAfter: 25,
+      freeMode: true
+    });
+})
